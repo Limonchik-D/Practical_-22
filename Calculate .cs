b1eec14ae3da1bad886dec1243b4940ac5f9e697
Limon <Limon@DAVID>
2025-02-09 19:19:25 +0200

    Добавьте файлы проекта.

---
+++ Calculate .cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical__22
+{/// <summary>
+/// Создать класс Calculate с указанными двумя полями (Поле 1: вещественное число, Поле 2: вещественное число) и методами: 
+/// конструкторы для инициализации объекта(без параметров, с одним параметром, с двумя параметрами); 
+/// функция формирования строки с информацией об объекте; функция обработки значений полей: вычислить корень квадратный из произведения чисел.
+/// </summary>
+    internal class Calculate
+    {
+        public double material;
+        public double material2;
+
+        public Calculate() //без параметров
+        {
+            material = 0;
+            material2 = 0;
+        }
+        public Calculate(double mtl) //С одним параметром
+        {
+            material = mtl;
+            material2 = 0;
+        }
+        public Calculate(double mtl, double mtl2)//С двумя параметрами
+        {
+            material = mtl;
+            material2 = mtl2;
+        }
+        public double Function()
+        {
+            double result = Math.Sqrt(material * material2);
+            return result >= 0 ? Math.Sqrt(result): double.NaN;
+        }
+        public override string ToString()
+        {
+            return $"Число 1: {material}, Число 2: {material2}, Корень произведения {Function()}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical__22
+{
+    internal class Program
+    {/// <summary>
+     /// Создать объекты класса: с использованием конструктора без параметров и присваивания значений полям объекта; 
+     /// с явной инициализацией; с использованием конструктора с двумя параметрами, введенными с клавиатуры.
+     /// </summary>
+        static void Main(string[] args)
+        {
+            Calculate calculate = new Calculate(); //конструктор без параметров и присваивание значений полям объекта
+            
+                calculate.material = 5;
+                calculate.material2 = 10;
+                Console.WriteLine(calculate);
+            Console.WriteLine();
+
+            Calculate calculate1 = new Calculate(4.5, 14.2); //явная инициализация
+            
+                Console.WriteLine(calculate1);
+            Console.WriteLine();
+
+            Console.Write("Введите число: ");
+            double material;
+            while(!double.TryParse(Console.ReadLine(), out material))
+            {
+                Console.Write("Ошибка! Введите число: ");
+            }
+
+            Console.Write("Введите число: ");
+            double material2;
+            while (!double.TryParse(Console.ReadLine(), out material2))
+            {
+                Console.Write("Ошибка! Введите число: ");
+            }    
+            Calculate calculate2 = new Calculate(material, material2); //в ручную
+            Console.WriteLine(calculate2);
+
+            Console.ReadKey();
+        }
+    }
+}
